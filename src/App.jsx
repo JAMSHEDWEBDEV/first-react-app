af4db63c93d5a73545efbe85facf231e55d6441a
JAMSHEDWEBDEV <jamsheddc60@gmail.com>
2023-06-24 13:32:10 +0600

    version 1.0

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Data(props){
+  return (
+    <div>
+      <h1>{props.content}</h1>
+    </div>
+  )
+}
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>React</h1>
+      <Data content="Hello JS"></Data>
+      <Data content="Hello React"></Data>
+    </>
+  )
+}
+
+export default App
